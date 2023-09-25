147f08d80c4c184aeac3307e0b1513c6cbffaca4
pranitmodi <51528295+pranitmodi@users.noreply.github.com>
2023-09-25 14:48:31 +0530

    did it

--- script.js
+++ script.js
@@ -7,16 +7,92 @@ let decrypt = document.querySelector("#option .decrypt")
 
 let todo = 0
 
-function asciiToEmoji(asciiValue) 
-{
-    return String.fromCodePoint(parseInt(asciiValue, 10));
-}
-function emojiToAscii(emoji) 
-{
-    const codePoint = emoji.codePointAt(0);
-    return codePoint.toString();
+function toEmoji(dec) 
+{ //map ascii printable characters to specific emoji faces
+    if (dec == 32) 
+    {
+      return (String.fromCodePoint(128169));
+    }
+    else if (dec == 33) 
+    {
+      return (String.fromCodePoint(129488));
+    }
+    else if (dec == 34) {
+      return (String.fromCodePoint(129402));
+    }
+    else if (dec == 35) {
+      return (String.fromCodePoint(129400));
+    }
+    else if (dec == 36) {
+      return (String.fromCodePoint(129303));
+    }
+    else if (dec <= 39) {
+      return (String.fromCodePoint(dec + 128547));
+    }
+    else if (dec <= 43) {
+      return (String.fromCodePoint(dec + 128537));
+    }
+    else if (dec <= 49) {
+      return (String.fromCodePoint(dec + 129252));
+    }
+    else if (dec <= 105) {
+      return (String.fromCodePoint(dec + 128462));
+    }
+    else if (dec <= 111) {
+      return (String.fromCodePoint(dec + 129206));
+    }
+    else if (dec <= 120) {
+      return (String.fromCodePoint(dec + 129207));
+    }
+    else 
+    {
+      return (String.fromCodePoint(dec + 129271));
+    }
 }
   
+function toCharacter(dec) 
+{
+    if (dec == 128169) 
+    {
+      return (String.fromCodePoint(32));
+    }
+    else if (dec == 129488) 
+    {
+      return (String.fromCodePoint(33));
+    }
+    else if (dec == 129402) 
+    {
+      return (String.fromCodePoint(34));
+    }
+    else if (dec == 129400) {
+      return (String.fromCodePoint(35));
+    }
+    else if (dec == 129303) {
+      return (String.fromCodePoint(36));
+    }
+    else if (dec >= 129392) {
+      return (String.fromCodePoint(dec - 129271));
+    }
+    else if (dec >= 129319) {
+      return (String.fromCodePoint(dec - 129207));
+    }
+    else if (dec >= 129312) {
+      return (String.fromCodePoint(dec - 129206));
+    }
+    else if (dec >= 129296) {
+      return (String.fromCodePoint(dec - 129252));
+    }
+    else if (dec >= 128584) {
+      return (String.fromCodePoint(dec - 128547));
+    }
+    else if (dec >= 128577) {
+      return (String.fromCodePoint(dec - 128537));
+    }
+    else 
+    {
+      return (String.fromCodePoint(dec - 128462));
+    }
+}
 
 function process()
 {
@@ -24,32 +100,46 @@ function process()
     let pass = document.querySelector("#card #password").value
 
     let clutter = ""
-    const str = data.split("")
-    
-    console.log("data:" + data)
 
     if(todo == 0)
     {
-        str.forEach(element => 
-        {   
-            clutter += `&#128${element.charCodeAt()}`
-        });
-        const str1 = clutter.split("")
-        str1.forEach(element => 
-        {   
-            console.log(element)
-        });
+        var cypher = CryptoJS.AES.encrypt(data,pass).toString();
+        var sliced = cypher.slice(10);//remove U2FsdGVkX1
+        for (var i = 0; i < sliced.length; i++) 
+        {
+            var dec = sliced.codePointAt(i);
+            clutter += toEmoji(dec);
+        }
     }
     else
     {
-        str.forEach(ele =>
-        {
-            console.log(ele.charCodeAt(0))
-        })
+      console.log("Doing DECRYPTION")
+      data.replace(/[0-9]/g, '');
+      console.log(data)
+      var cypher = 'U2FsdGVkX1';
+      try 
+      {
+        console.log("okay")
+          //build cyphertext
+          for (var i = 0; i < data.length/2; i++) 
+          {
+              var dec = data.codePointAt(i * 2);
+              cypher = cypher + toCharacter(dec);
+          }
+          //decrypt
+          var bytes  = CryptoJS.AES.decrypt(cypher,pass);
+          console.log("bytes: " + bytes)
+          clutter = bytes.toString(CryptoJS.enc.Utf8);
+          console.log("clutter" + clutter)
+      } 
+      catch (error) 
+      {
+          clutter = '';
+      }
     }
+    
+    result_ans.innerText = clutter
 
-    console.log(clutter)
-    result_ans.innerHTML = String.fromCharCode(568344)
 }
 
 encrypt.addEventListener("click",function()
@@ -82,6 +172,8 @@ encrypt.addEventListener("click",function()
 
     result.style.display = "none"
 
+    document.querySelector("#card #message").value = ""
+    document.querySelector("#card #password").value = ""
     document.querySelector("#text1").innerText = `1. Type a message`
     document.querySelector("#text2").innerText = `2. Set a password`
     document.querySelector("#text3").innerText = `3. Encrypt message`
@@ -93,6 +185,7 @@ encrypt.addEventListener("click",function()
 decrypt.addEventListener("click",function()
 {
     todo = 1
+    console.log("Decrypt---" + todo)
     console.log("inside decrypt")
     gsap.to(decrypt,
         {
@@ -120,11 +213,13 @@ decrypt.addEventListener("click",function()
 
     result.style.display = "none"
 
+    document.querySelector("#card #message").value = ""
+    document.querySelector("#card #password").value = ""
     document.querySelector("#text1").innerText = `1. Type the encrypted message`
     document.querySelector("#text2").innerText = `2. Enter the password`
     document.querySelector("#text3").innerText = `3. Decrypt Emojis`
     document.querySelector("#text4").innerText = `Decrypted Text:`
-    document.querySelector("#main-btn").innerHTML = `<i class="ri-lock-unlock-line"></i> Decrypt Text`
+    document.querySelector("#main-btn").innerHTML = `<i class="ri-lock-unlock-line"></i> Decrypt Emojis`
     result_ans.innerText = ""
 })
 
